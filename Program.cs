28602bd31de3237d0ec4f1732aaeab99f6638b7b
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-24 20:45:42 +0530

    [Rupali]ADDED: Welcome to GenericsProblem

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
